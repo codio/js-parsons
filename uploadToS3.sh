3e969e1546152126594e88ad7d7b7883dc7845de
Dmitrii Suchkov <dsuchkov@codio.com>
2025-10-27 10:57:31 +0000

    Github-AWS openId (#12)
    see https://bugtracker.codiodev.com/issue/codio-17207

--- uploadToS3.sh
+++ uploadToS3.sh
@@ -7,9 +7,7 @@ joinByChar() {
   echo "$*"
 }
 
-s3Key=$1
-s3Secret=$2
-tag=$3
+tag=$1
 folderArr=("guides" "parsons")
 folder=$(joinByChar "/", "${folderArr[@]}")
 folderEscaped=$(joinByChar "\/", "${folderArr[@]}")
@@ -49,16 +47,9 @@ uploadFile () {
   fName="${file#./}"
   contentType=$2
   bucket="codio-assets"
-  resource="/${bucket}/${folder}/${tag}/${fName}"
-  dateValue=$(date -R)
-  stringToSign="PUT\n\n${contentType}\n${dateValue}\n${resource}"
-  signature=$(echo -en "${stringToSign}" | openssl sha1 -hmac "${s3Secret}" -binary | base64)
-  curl -X PUT -T "${file}" \
-    -H "Host: ${bucket}.s3.amazonaws.com" \
-    -H "Date: ${dateValue}" \
-    -H "Content-Type: ${contentType}" \
-    -H "Authorization: AWS ${s3Key}:${signature}" \
-    https://${bucket}.s3.amazonaws.com/"${folder}"/"${tag}"/"${fName}" || exit 1
+  resource="s3://${bucket}/${folder}/${tag}/${fName}"
+  
+  aws s3 cp "${file}" "${resource}" --cache-control no-cache --content-type "${contentType}"
 }
 
 for file in "${files[@]}"
